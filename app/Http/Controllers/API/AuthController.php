627eb0043c9716d0448e4620ae8c539305cd4f9e
Dayan Perez <dayan.perez9012@gmail.com>
2021-04-14 00:29:30 -0300

    First commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $validateData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validateData['password'] = Hash::make($validateData['password']);
+        $user = User::create($validateData);
+        $accessToken = $user->createToken('authToken')->accessToken;
+        return response(['user' => $user, 'access_token' => $accessToken], 201);
+    }
+
+    public function login(Request $request) {
+        $loginData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if(!auth()->attempt($loginData)) {
+            return response(['message' => 'This User does not exist, check your details'], 400);
+        }
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+}
